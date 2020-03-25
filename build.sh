d7edc7a5602372e0a482f05320df4ce4fbe0198a
Marek Läll <marek.lall@eesti.ee>
2020-03-25 16:54:34 +0200

    build.sh added

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+# gradle
+if [[ -z "${GRADLE}" ]] ; then
+  export GRADLE="../gradle/bin/gradle --no-daemon"
+fi
+echo  "# GRADLE=${GRADLE}"
+
+#
+# oracle environment (ORACLE_HOME)
+if [[ -z "${ORACLE_HOME}" ]] ; then
+  source "${HOME}/ora190.env"
+fi
+export ORACLE_HOME_190="${ORACLE_HOME}"
+echo  "# ORACLE_HOME_190=${ORACLE_HOME_190}"
+
+#
+# java environment
+echo  "${JAVA_HOME}"
+if [[ -z "${JAVA_HOME}" ]] ; then
+  export JAVA_HOME="${HOME}/tools/jdk1.8"
+fi
+echo  "# JAVA_HOME=${JAVA_HOME}"
+
+
+# clean
+${GRADLE} clean
+# build
+${GRADLE} build
+
+# end
+
